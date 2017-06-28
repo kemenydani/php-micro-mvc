0f31f7bd7f9404a09d4b5e04ed10914bf2bafccb
Dani <kemenydani93@gmail.com>
2017-06-28 19:55:19 +0200

    Outsourced the creation of controllers and calling methods to a Factory

--- Core/Router.php
+++ Core/Router.php
@@ -9,44 +9,36 @@ namespace Core;
  */
 class Router {
 	
-	const _CONTROLLER_ROOT_ = 'Controllers\\';
-	const _DEFAULT_CONTROLLER_ = 'Home';
 	const _DEFAULT_METHOD_ = 'index';
 	
-	public static $_controller;
-	public static $_method;
+	public $controllerFactory;
+	public $controller;
+	protected static $_instance;
+	
+	public static function getInstance(){
+		if(self::$_instance === null){
+			self::$_instance = new Router();
+		}
+		return self::$_instance;
+	}
 	
 	public function __construct(){
-		echo '1';
+		$this->controllerFactory = new ControllerFactory();
 	}
 	
-	public static function init($url){
+	public function load(Request $url){
 
-		$controller_name = isset($url['page']) ? $url['page'] : static::_DEFAULT_CONTROLLER_;
-		$controller_path = static::_CONTROLLER_ROOT_ . ucfirst($controller_name);
-
-		if(file_exists($controller_path . '.php')){
-			self::$_controller = new $controller_path;
+		if($this->controllerFactory::exists($url::get('page'))){
+			$this->controllerFactory::make($url::get('page'));
 		} else {
-			$default_controller_path = static::_CONTROLLER_ROOT_ . static::_DEFAULT_CONTROLLER_;
-			self::$_controller = new $default_controller_path;
+			$this->controllerFactory::make('home');
 		}
 		
-		unset($url['page']);
-		
-		self::call_method($url);
-	}
-	
-	public static function call_method($url){
-		
-		if(isset($url['action']) && method_exists(self::$_controller, strtolower($url['action']))){
-			self::$_method = strtolower($url['action']);
+		if($this->controllerFactory::method_exists($url::get('action'))){
+			$this->controllerFactory::call_method($url::get('action'));
 		} else {
-			self::$_method = static::_DEFAULT_METHOD_;
+			$this->controllerFactory::call_method('index');
 		}
-		
-		unset($url['action']);
-		call_user_func([self::$_controller, self::$_method], $url);
+
 	}
-	
 }
